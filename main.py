f467616e44a8392207dde3d21d0a8109c6db5a44
talasbek <algazievtalas@gmail.com>
2023-12-13 23:13:45 +0600

    add new redoc

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-a = 'talasbek'
-for i in a:
-    print(i)
+# number_1 = int(input('цифра от 2 - 10 '))
+# x = number_1 * 2 + 5
+# birthday = int(input('год рождение '))
+# ttrue = int(input('в этом году был не рлжденич?'))
+# result = x * 50 + 1772 + ttrue - birthday, number_1
+# lists = []
+# lists.append(x)
+# print(lists)
+
+# f = 3 * 2 + 5
+# print(f * 50 + 1772 + 1 - 2006)
